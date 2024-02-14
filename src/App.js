55cd31d898e3b69aee2a8049f10652363c4c290d
Kenneth Parrales <kenneth.parrales@gmail.com>
2024-02-14 12:54:59 -0600

    navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import Home from "./component/Home";
 import ColoresView from "./views/Colores.js";
@@ -30,7 +30,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<LoginView />} />
+          <Route path='/' element={<Navigate to="/login" />} />
           <Route path='/index' element={<Home />} />
           <Route path='/configuracion' element={<ConfigView />} />
           <Route path='/colores' element={<ColoresView />} />
